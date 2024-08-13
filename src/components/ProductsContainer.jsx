3622f4b0c3231ea9cc713320c9324094a281569b
Mohamed Hamada <moha2016226@gmail.com>
2024-08-13 12:34:42 -0700

    create a List of Products

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
 const ProductsContainer = () => {
-  return <div>Products Container</div>;
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  );
 };
 
 export default ProductsContainer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Filters, ProductsContainer, PaginationContainer } from "../components";
+import { customFetch } from "../utils";
+const url = "/products";
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  const meta = response.data.meta;
+  console.log(response);
+  return { products, meta };
+};
 const Products = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
+};
 
-export default Products
+export default Products;
