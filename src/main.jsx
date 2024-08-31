d0492fb5fcaa70277b9d80050075ccf032ec5300
Mohamed Hamada <moha2016226@gmail.com>
2024-08-31 12:32:47 -0700

    setup redux toolkit state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./store.jsx";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer position="top-center" />
+  </Provider>
+);
