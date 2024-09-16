7c9983f6ab21545a5cf0d11f95300e2fffe4dc5d
Mohamed Hamada <moha2016226@gmail.com>
2024-09-16 08:33:27 -0700

    changes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Filters, ProductsContainer, PaginationContainer } from "../components";
 import { customFetch } from "../utils";
 const url = "/products";
+
 const allProductsQuery = (queryParams) => {
   const { search, category, company, sort, price, shipping, page } =
     queryParams;
@@ -25,16 +26,16 @@ export const loader =
     const params = Object.fromEntries([
       ...new URL(request.url).searchParams.entries(),
     ]);
-    // console.log(params);
+
     const response = await queryClient.ensureQueryData(
       allProductsQuery(params)
     );
+
     const products = response.data.data;
     const meta = response.data.meta;
-   
-
     return { products, meta, params };
   };
+
 const Products = () => {
   return (
     <>
